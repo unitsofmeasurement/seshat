36ec1255df50e348ce12ae392498448b762c73c0
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2019-09-14 17:47:30 +0200

    Move description from module-info to package-info since the module info description is not yet included in generated javadoc.

--- src/main/java/tech/uom/seshat/package-info.java
+++ src/main/java/tech/uom/seshat/package-info.java
@@ -15,7 +15,7 @@
  */
 
 /**
- * Units of measurements and their formatters.
+ * Implementation of Units of Measurement API defined by JSR 363.
  * The {@link tech.uom.seshat.Units} class provides static constants
  * for about 50 units of measurement including all the SI base units
  * ({@linkplain tech.uom.seshat.Units#METRE    metre},
@@ -48,8 +48,45 @@
  *   <li>{@linkplain tech.uom.seshat.Quantities}
  *       as a {@code double} value value associated to a {@code Unit} instance.</li>
  *   <li>Parsers and formatters
- *      ({@link tech.uom.seshat.UnitFormat})</li>
+ *      ({@link tech.uom.seshat.UnitFormat}).</li>
  * </ul>
+ *
+ * <div class="section">Arithmetic operations</div>
+ * <p>Seshat supports arithmetic operations on units and on quantities.
+ * The unit (including SI prefix) and the quantity type resulting from
+ * those arithmetic operations are automatically inferred.
+ * For example this line of code:</p>
+ *
+ * <blockquote><pre>{@code System.out.println( Units.PASCAL.multiply(1000) );}</pre></blockquote>
+ *
+ * <p>prints <cite>"kPa"</cite>, i.e. the kilo prefix has been automatically applied
+ * (SI prefixes are applied on SI units only, not on other systems).
+ * Other example:</p>
+ *
+ * <blockquote><pre>{@code Force  f = Quantities.create(4, Units.NEWTON);
+ *Length d = Quantities.create(6, Units.MILLIMETRE);
+ *Time   t = Quantities.create(3, Units.SECOND);
+ *Quantity<?> e = f.multiply(d).divide(t);
+ *System.out.println(e);
+ *System.out.println("Is instance of Power: " + (e instanceof Power));}</pre></blockquote>
+ *
+ * <p>prints {@code "8 mW"} and {@code "Is instance of Power: true"},
+ * i.e. Seshat detects that the result of N⋅m∕s is Watt,
+ * inherits the milli prefix from millimetre and creates an instance
+ * of {@link javax.measure.quantity.Power}, not just {@code Quantity<Power>} (the generic parent).</p>
+ *
+ * <div class="section">Parsing and formatting</div>
+ * <p>{@linkplain tech.uom.seshat.Units#valueOf(String) Parsing} and formatting use Unicode symbols by default, as in "µg/m²".
+ * Parenthesis are recognized at parsing time and used for denominators at formatting time, as in "kg/(m²⋅s)".
+ * While uncommon, Seshat accepts fractional powers as in "m^⅔".
+ * Some sentences like <cite>"100 feet"</cite>, <cite>"square metre"</cite> and <cite>"degree Kelvin"</cite>
+ * are also recognized at parsing time.</p>
+ *
+ * <div class="section">Source</div>
+ * <p>Seshat is a subset of <a href="http://sis.apache.org/">Apache Spatial Information System (SIS)</a>
+ * library keeping only the classes required for JSR 363, with geospatial-specific functionalities omitted.
+ * The omitted functionalities are {@code Salinity}, {@code AngularVelocity} and {@code ScaleRateOfChange}
+ * quantities, sigma-level units, sexagesimal units and EPSG codes.</p>
  *
  * @author  Martin Desruisseaux (MPO, IRD, Geomatys)
  * @version 1.0
--- src/main/java_10/module-info.java
+++ src/main/java_10/module-info.java
@@ -13,44 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import javax.measure.spi.ServiceProvider;
 
 
 /**
  * Implementation of Units of Measurement API defined by JSR 363.
- * Seshat is a subset of <a href="http://sis.apache.org/">Apache Spatial Information System (SIS)</a>
- * library keeping only the classes required for JSR 363, with geospatial-specific functionalities omitted.
- * The omitted functionalities are {@code Salinity}, {@code AngularVelocity} and {@code ScaleRateOfChange}
- * quantities, sigma-level units, sexagesimal units and EPSG codes.
- *
- * <p>Seshat supports arithmetic operations on units and on quantities.
- * The unit (including SI prefix) and the quantity type resulting from
- * those arithmetic operations are automatically inferred.
- * For example this line of code:</p>
- *
- * <blockquote><pre>{@code System.out.println( Units.PASCAL.multiply(1000) );}</pre></blockquote>
- *
- * <p>prints <cite>"kPa"</cite>, i.e. the kilo prefix has been automatically applied
- * (SI prefixes are applied on SI units only, not on other systems).
- * Other example:</p>
- *
- * <blockquote><pre>{@code Force  f = Quantities.create(4, Units.NEWTON);
- *Length d = Quantities.create(6, Units.MILLIMETRE);
- *Time   t = Quantities.create(3, Units.SECOND);
- *Quantity<?> e = f.multiply(d).divide(t);
- *System.out.println(e);
- *System.out.println("Instance of Power: " + (e instanceof Power));}</pre></blockquote>
- *
- * <p>prints <cite>"8 mW"</cite> and <cite>"Instance of Power: true"</cite>,
- * i.e. Seshat detects that the result of N⋅m∕s is Watt,
- * inherits the milli prefix from millimetre and creates an instance
- * of {@link javax.measure.quantity.Power}, not just {@code Quantity<Power>} (the generic parent).</p>
- *
- * {@linkplain tech.uom.seshat.Units#valueOf(String) Parsing} and formatting use Unicode symbols by default, as in µg/m².
- * Parenthesis are recognized at parsing time and used for denominators at formatting time, as in kg/(m²⋅s).
- * While uncommon, Seshat accepts fractional powers as in m^⅔.
- * Some sentences like <cite>"100 feet"</cite>, <cite>"square metre"</cite> and <cite>"degree Kelvin"</cite>
- * are also recognized at parsing time.
  *
  * @author  Martin Desruisseaux (MPO, IRD, Geomatys)
  * @version 1.0
@@ -66,5 +32,5 @@ module tech.uom.seshat {
      * public in Apache SIS if needed.
      */
 
-    provides ServiceProvider with tech.uom.seshat.UnitServices;
+    provides javax.measure.spi.ServiceProvider with tech.uom.seshat.UnitServices;
 }
