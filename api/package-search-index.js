9418d1c61ce5599b6ec9977c2d03a4a1e9fe854f
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2021-03-05 12:43:00 +0100

    Regenerate Javadoc with JDK 15. Content should be the same.

--- api/package-search-index.js
+++ api/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"tech.uom.seshat"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"tech.uom.seshat"}];updateSearchResults();
--- api/type-search-index.js
+++ api/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"tech.uom.seshat","l":"Quantities"},{"p":"tech.uom.seshat","l":"UnitFormat.Style"},{"p":"tech.uom.seshat","l":"UnitFormat"},{"p":"tech.uom.seshat","l":"Units"},{"p":"tech.uom.seshat","l":"UnitServices"}]
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"tech.uom.seshat","l":"Quantities"},{"p":"tech.uom.seshat","l":"UnitFormat.Style"},{"p":"tech.uom.seshat","l":"UnitFormat"},{"p":"tech.uom.seshat","l":"Units"},{"p":"tech.uom.seshat","l":"UnitServices"}];updateSearchResults();
