a3c0fb294460c1a3d6c4fcbeb791f0396aa6fb25
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2022-12-29 15:02:32 +0100

    Move some documentation from package-info to module-info.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -17,9 +17,73 @@
 
 /**
  * Implementation of Units of Measurement API defined by JSR 385.
+ * The {@link tech.uom.seshat.Units} class provides static constants
+ * for about 50 units of measurement including all the SI base units
+ * ({@linkplain tech.uom.seshat.Units#METRE    metre},
+ *  {@linkplain tech.uom.seshat.Units#KILOGRAM kilogram}
+ *  {@linkplain tech.uom.seshat.Units#SECOND   second},
+ *  {@linkplain tech.uom.seshat.Units#AMPERE   ampere},
+ *  {@linkplain tech.uom.seshat.Units#KELVIN   kelvin},
+ *  {@linkplain tech.uom.seshat.Units#MOLE     mole} and
+ *  {@linkplain tech.uom.seshat.Units#CANDELA  candela})
+ * together with some derived units
+ * ({@linkplain tech.uom.seshat.Units#SQUARE_METRE      square metre},
+ *  {@linkplain tech.uom.seshat.Units#CUBIC_METRE       cubic metre},
+ *  {@linkplain tech.uom.seshat.Units#METRES_PER_SECOND metres per second},
+ *  {@linkplain tech.uom.seshat.Units#HERTZ             hertz},
+ *  {@linkplain tech.uom.seshat.Units#PASCAL            pascal},
+ *  {@linkplain tech.uom.seshat.Units#NEWTON            newton},
+ *  {@linkplain tech.uom.seshat.Units#JOULE             joule},
+ *  {@linkplain tech.uom.seshat.Units#WATT              watt},
+ *  {@linkplain tech.uom.seshat.Units#TESLA             tesla},
+ *  <i>etc.</i>)
+ * and some dimensionless units
+ * ({@linkplain tech.uom.seshat.Units#RADIAN    radian},
+ *  {@linkplain tech.uom.seshat.Units#STERADIAN steradian},
+ *  {@linkplain tech.uom.seshat.Units#PIXEL     pixel},
+ *  {@linkplain tech.uom.seshat.Units#UNITY     unity}).
+ *
+ * <h2>Arithmetic operations</h2>
+ * Seshat supports arithmetic operations on units and on quantities.
+ * The unit (including SI prefix) and the quantity type resulting from
+ * those arithmetic operations are automatically inferred.
+ * For example this line of code:
+ *
+ * {@snippet lang="java" :
+ *     System.out.println( Units.PASCAL.multiply(1000) );
+ *     }
+ *
+ * prints <cite>"kPa"</cite>, i.e. the kilo prefix has been automatically applied
+ * (SI prefixes are applied on SI units only, not on other systems).
+ * Other example:
+ *
+ * {@snippet lang="java" :
+ *     Force  f = Quantities.create(4, Units.NEWTON);
+ *     Length d = Quantities.create(6, Units.MILLIMETRE);
+ *     Time   t = Quantities.create(3, Units.SECOND);
+ *     Quantity<?> e = f.multiply(d).divide(t);
+ *     System.out.println(e);
+ *     System.out.println("Is instance of Power: " + (e instanceof Power));
+ *     }
+ *
+ * prints {@code "8 mW"} and {@code "Is instance of Power: true"},
+ * i.e. Seshat detects that the result of N⋅m∕s is Watt,
+ * inherits the milli prefix from millimetre and creates an instance
+ * of {@link javax.measure.quantity.Power}, not just {@code Quantity<Power>} (the generic parent).
+ *
+ * <h2>Parsing and formatting</h2>
+ * {@linkplain tech.uom.seshat.Units#valueOf(String) Parsing} and formatting use Unicode symbols by default, as in "µg/m²".
+ * Parenthesis are recognized at parsing time and used for denominators at formatting time, as in "kg/(m²⋅s)".
+ * While uncommon, Seshat accepts fractional powers as in "m^⅔".
+ * Some sentences like <cite>"100 feet"</cite>, <cite>"square metre"</cite> and <cite>"degree Kelvin"</cite>
+ * are also recognized at parsing time.
+ *
+ * <h2>Source</h2>
+ * Seshat is a subset of <a href="https://sis.apache.org/">Apache Spatial Information System (SIS)</a>
+ * library keeping only the classes required for JSR 385 implementation.
  *
  * @author  Martin Desruisseaux (MPO, IRD, Geomatys)
- * @version 1.2
+ * @version 1.3
  * @since   1.2
  */
 module tech.uom.seshat {
--- src/main/java/tech/uom/seshat/package-info.java
+++ src/main/java/tech/uom/seshat/package-info.java
@@ -16,16 +16,20 @@
 
 /**
  * Implementation of Units of Measurement API defined by JSR 385.
- * The {@link tech.uom.seshat.Units} class provides static constants
- * for about 50 units of measurement including all the SI base units
+ * This package provides:
+ *
+ * <ul>
+ *   <li>The {@link tech.uom.seshat.Units} class with static constants for about 50 units of measurement including:
+ *     <ul>
+ *       <li>all the SI base units
  * ({@linkplain tech.uom.seshat.Units#METRE    metre},
  *  {@linkplain tech.uom.seshat.Units#KILOGRAM kilogram}
  *  {@linkplain tech.uom.seshat.Units#SECOND   second},
  *  {@linkplain tech.uom.seshat.Units#AMPERE   ampere},
  *  {@linkplain tech.uom.seshat.Units#KELVIN   kelvin},
  *  {@linkplain tech.uom.seshat.Units#MOLE     mole} and
- *  {@linkplain tech.uom.seshat.Units#CANDELA  candela})
- * together with some derived units
+ *  {@linkplain tech.uom.seshat.Units#CANDELA  candela}),
+ *       </li><li>some derived units
  * ({@linkplain tech.uom.seshat.Units#SQUARE_METRE      square metre},
  *  {@linkplain tech.uom.seshat.Units#CUBIC_METRE       cubic metre},
  *  {@linkplain tech.uom.seshat.Units#METRES_PER_SECOND metres per second},
@@ -35,60 +39,25 @@
  *  {@linkplain tech.uom.seshat.Units#JOULE             joule},
  *  {@linkplain tech.uom.seshat.Units#WATT              watt},
  *  {@linkplain tech.uom.seshat.Units#TESLA             tesla},
- *  <i>etc.</i>)
- * and some dimensionless units
+ *  <i>etc.</i>),
+ *       </li><li>and some dimensionless units
  * ({@linkplain tech.uom.seshat.Units#RADIAN    radian},
  *  {@linkplain tech.uom.seshat.Units#STERADIAN steradian},
  *  {@linkplain tech.uom.seshat.Units#PIXEL     pixel},
  *  {@linkplain tech.uom.seshat.Units#UNITY     unity}).
- *
- * In relation to units of measurement, this package also defines:
- *
- * <ul>
+ *       </li>
+ *     </ul>
+ *   </li>
  *   <li>{@linkplain tech.uom.seshat.Quantities}
  *       as a {@code double} value value associated to a {@code Unit} instance.</li>
  *   <li>Parsers and formatters
- *      ({@link tech.uom.seshat.UnitFormat}).</li>
+ *      ({@link tech.uom.seshat.UnitFormat} and {@link tech.uom.seshat.QuantityFormat}).</li>
  * </ul>
  *
- * <h2>Arithmetic operations</h2>
  * Seshat supports arithmetic operations on units and on quantities.
  * The unit (including SI prefix) and the quantity type resulting from
  * those arithmetic operations are automatically inferred.
- * For example this line of code:
- *
- * {@snippet lang="java" :
- *     System.out.println( Units.PASCAL.multiply(1000) );
- * }
- *
- * prints <cite>"kPa"</cite>, i.e. the kilo prefix has been automatically applied
- * (SI prefixes are applied on SI units only, not on other systems).
- * Other example:
- *
- * {@snippet lang="java" :
- *     Force  f = Quantities.create(4, Units.NEWTON);
- *     Length d = Quantities.create(6, Units.MILLIMETRE);
- *     Time   t = Quantities.create(3, Units.SECOND);
- *     Quantity<?> e = f.multiply(d).divide(t);
- *     System.out.println(e);
- *     System.out.println("Is instance of Power: " + (e instanceof Power));
- * }
- *
- * prints {@code "8 mW"} and {@code "Is instance of Power: true"},
- * i.e. Seshat detects that the result of N⋅m∕s is Watt,
- * inherits the milli prefix from millimetre and creates an instance
- * of {@link javax.measure.quantity.Power}, not just {@code Quantity<Power>} (the generic parent).
- *
- * <h2>Parsing and formatting</h2>
- * {@linkplain tech.uom.seshat.Units#valueOf(String) Parsing} and formatting use Unicode symbols by default, as in "µg/m²".
- * Parenthesis are recognized at parsing time and used for denominators at formatting time, as in "kg/(m²⋅s)".
- * While uncommon, Seshat accepts fractional powers as in "m^⅔".
- * Some sentences like <cite>"100 feet"</cite>, <cite>"square metre"</cite> and <cite>"degree Kelvin"</cite>
- * are also recognized at parsing time.
- *
- * <h2>Source</h2>
- * Seshat is a subset of <a href="http://sis.apache.org/">Apache Spatial Information System (SIS)</a>
- * library keeping only the classes required for JSR 385 implementation.
+ * See the module summary page for code examples.
  *
  * @author  Martin Desruisseaux (MPO, IRD, Geomatys)
  * @version 1.3
